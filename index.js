dcb9a047a2803b84ab9b8d415ce5d0f1a6903fd9
suyash-thakur <500067151@stu.upes.ac.in>
2023-04-15 15:08:44 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Gen AI listening on port ${port}!`));
---
+++ services/gpt.js
@@ -0,0 +1,46 @@
+// Create endpoints for gpt completion and dalle generation
+const { Configuration, OpenAIApi } = require('openai');
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    basePath: 'https://api.openai.com/v1'
+});
+
+const openai = new OpenAIApi(config);
+
+const chatCompletion = async ({ messages }) => { 
+    try {
+        if(!messages || messages.length === 0) throw new Error('No messages provided');
+        const config = {
+            model: 'gpt-3.5-turbo',
+            messages,
+        };
+        const response = await openai.completion(config);
+        return response.data.choices[0].message.content;
+    } catch (error) {
+        console.error(error);
+        return '';
+    }
+}
+
+
+const imageGeneration = async ({ prompt }) => {
+    try {
+        if (!prompt) throw new Error('No prompt provided');
+        const config = {
+            prompt,
+            n: 1,
+            size: '512x512',
+    }
+        const response = await openai.imageCompletion(config);
+        return response.data.images[0];
+    } catch (error) {
+        console.error(error);
+        return '';
+    }
+}
+
+module.exports = {
+    chatCompletion,
+    imageGeneration,
+}
